7ec9c71e54ff53a1ec9b0b92786585ac0f193788
Bhanu Pratap Singh <98203615+SINGHBP29@users.noreply.github.com>
2022-10-29 13:17:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('Hello Crew,This website is Created for Your Problem which is facing early days You submit your camplain/Problem here and solved by your owner')
+if (alert){
+    alert("Plaese login first")
+}
+if (message){
+
+document.write("hello")
+}
+
+document.getElementById("login").innerHTML="Login"
